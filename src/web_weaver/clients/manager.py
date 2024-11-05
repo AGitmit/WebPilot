46ed3c4ebb446624254d1d8a4179fe86f29ac5ae
Amit Nakash <87775966+AGitmit@users.noreply.github.com>
2024-11-06 00:30:10 +0200

    rename manager module

--- src/web_weaver/clients/manager.py
+++ src/web_weaver/clients/manager.py
@@ -14,14 +14,7 @@ class BrowserManager:
         self._pool = {}
         self.max_browsers = conf.max_browsers_cap
 
-    def _check_chromium(self):
-        if not pyppeteer.chromium_downloader.check_chromium():
-            pyppeteer.chromium_downloader.download_chromium()
-
-    def get_browser(self, browser_id: int) -> Optional[pyppeteer.browser.Browser]:
-        return self._pool.get(browser_id)
-
-    async def create_browser(self) -> pyppeteer.browser.Browser:
+    def create_browser(self) -> pyppeteer.browser.Browser:
         if len(self._pool) >= self.max_browsers:
             raise BrowserPoolCapacityReachedError(
                 f"Max number of browsers in pool reached: {self.max_browsers}"
@@ -29,3 +22,9 @@ class BrowserManager:
 
         self._pool[len(self._pool)] = Browser(len(self._pool))
         return self._pool[len(self._pool)]
+    
+    def remove_browser(self, browser_id: int) -> None:
+        del self._pool[browser_id]
+
+    def get_browser(self, browser_id: int) -> Optional[pyppeteer.browser.Browser]:
+        return self._pool.get(browser_id)
