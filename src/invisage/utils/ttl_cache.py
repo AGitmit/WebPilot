4d74a8b49e89162c67f8f0c31d74626cd9d397a0
Amit Nakash <87775966+AGitmit@users.noreply.github.com>
2024-11-08 17:02:47 +0200

    Create ttl_cache.py

---
+++ src/invisage/utils/ttl_cache.py
@@ -0,0 +1,33 @@
+import pydantic as pyd
+import cachetools
+
+from typing import Union
+from invisage.schemas.constants.cache import CacheProvider
+from invisage.config import config as conf
+
+
+class TTLCache:
+    def __init__(self) -> None:
+        self.cache = self._init_cache()
+        self._max_items = conf.max_cached_items
+        self._ttl = conf.cache_ttl
+
+    def _init_cache(self) -> Union[cachetools.TTLCache]:
+        if self._provider is None:
+            match conf.cache_provider:
+                case CacheProvider.IN_MEMORY:
+                    self._provider = cachetools.TTLCache(
+                        maxsize=conf.max_cached_items, ttl=conf.cache_ttl
+                    )
+                case _:
+                    raise ValueError("Unknown cache provider")
+        return self._provider
+
+    def get_item(self, key: str):
+        return self.cache.__getitem__(key)
+
+    def set_item(self, key: str, value: pyd.BaseModel) -> None:
+        self.cache.__setitem__(key, value)
+
+    def delete_item(self, key: str) -> None:
+        self.cache.__delitem__(key)
