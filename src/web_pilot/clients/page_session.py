0c14692247ba4b5f8433600b571db25c2bb7c79a
Amit Nakash <87775966+AGitmit@users.noreply.github.com>
2024-11-16 09:40:19 +0200

    expand page actions

--- src/web_pilot/clients/page_session.py
+++ src/web_pilot/clients/page_session.py
@@ -18,6 +18,17 @@ from web_pilot.utils.sessions import (
     perform_action_restoreSnapshot,
     perform_action_evaluate,
     perform_action_extractPageContents,
+    perform_action_exposeFunction,
+    perform_action_removeFunction,
+    perform_action_setViewport,
+    perform_action_evaluateOnNewDocument,
+    perform_action_setCookie,
+    perform_action_deleteCookie,
+    perform_action_evaluateHandle,
+    perform_action_addScriptTag,
+    perform_action_removeScriptTag,
+    perform_action_setGeoLocation,
+    perform_action_clearGeolocation,
 )
 
 
@@ -52,38 +63,74 @@ class PageSession:
     @pyd.validate_arguments
     @log_execution_metrics
     async def perform_page_action(self, action: PageActionType, **kwargs) -> Any:
+        match action:
+            case PageActionType.CLICK:
+                func_to_call = perform_action_click
+
+            case PageActionType.AUTHENTICATE:
+                func_to_call = perform_action_authenticate
+
+            case PageActionType.SET_USER_AGENT:
+                func_to_call = perform_action_setUserAgent
+
+            case PageActionType.SCREENSHOT:
+                func_to_call = perform_action_screenshot
+
+            case PageActionType.GOTO:
+                func_to_call = perform_action_goto
+
+            case PageActionType.GO_BACK:
+                func_to_call = perform_action_goBack
+
+            case PageActionType.GO_FORWARD:
+                func_to_call = perform_action_goForward
+
+            case PageActionType.SAVE_SNAPSHOT:
+                func_to_call = perform_action_saveSnapshot
+
+            case PageActionType.RESTORE_SNAPSHOT:
+                func_to_call = perform_action_restoreSnapshot
+
+            case PageActionType.EVALUATE:
+                func_to_call = perform_action_evaluate
+
+            case PageActionType.EXTRACT_PAGE_CONTENTS:
+                func_to_call = perform_action_extractPageContents
+
+            case PageActionType.EXPOSE_FUNCTION:
+                func_to_call = perform_action_exposeFunction
+
+            case PageActionType.REMOVE_FUNCTION:
+                func_to_call = perform_action_removeFunction
+
+            case PageActionType.SET_VIEWPORT:
+                func_to_call = perform_action_setViewport
+
+            case PageActionType.SET_GEOLOCATION:
+                func_to_call = perform_action_setGeoLocation
+
+            case PageActionType.CLEAR_GEOLOCATION:
+                func_to_call = perform_action_clearGeolocation
+
+            case PageActionType.ADD_SCRIPT_TAG:
+                func_to_call = perform_action_addScriptTag
+
+            case PageActionType.REMOVE_SCRIPT_TAG:
+                func_to_call = perform_action_removeScriptTag
+
+            case PageActionType.EVALUATE_HANDLE:
+                func_to_call = perform_action_evaluateHandle
+
+            case PageActionType.EVALUATE_ON_NEW_DOCUMENT:
+                func_to_call = perform_action_evaluateOnNewDocument
+
+            case PageActionType.SET_COOKIE:
+                func_to_call = perform_action_setCookie
+
+            case PageActionType.REMOVE_COOKIE:
+                func_to_call = perform_action_deleteCookie
+
         try:
-            match action:
-                case PageActionType.CLICK:
-                    func_to_call = perform_action_click
-
-                case PageActionType.AUTHENTICATE:
-                    func_to_call = perform_action_authenticate
-
-                case PageActionType.SET_USER_AGENT:
-                    func_to_call = perform_action_setUserAgent
-
-                case PageActionType.SCREENSHOT:
-                    func_to_call = perform_action_screenshot
-
-                case PageActionType.GOTO:
-                    func_to_call = perform_action_goto
-
-                case PageActionType.GO_BACK:
-                    func_to_call = perform_action_goBack
-
-                case PageActionType.GO_FORWARD:
-                    func_to_call = perform_action_goForward
-
-                case PageActionType.SAVE_SNAPSHOT:
-                    func_to_call = perform_action_saveSnapshot
-
-                case PageActionType.RESTORE_SNAPSHOT:
-                    func_to_call = perform_action_restoreSnapshot
-
-                case PageActionType.EVALUATE:
-                    func_to_call = perform_action_evaluate
-
             return await func_to_call(self._page, **kwargs)
             # return await perform_action_extractPageContents(self._page)
 
