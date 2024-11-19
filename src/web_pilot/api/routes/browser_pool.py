f6637b525206d679e5f6801851dcc3d03e8ef404
Amit Nakash <87775966+AGitmit@users.noreply.github.com>
2024-11-19 22:01:21 +0200

    remove manual scaling routes

--- src/web_pilot/api/routes/browser_pool.py
+++ src/web_pilot/api/routes/browser_pool.py
@@ -52,39 +52,3 @@ async def get_pool(pool_id: str):
 )
 async def delete_pool(pool_id: str, force: bool = Query(default=False)):
     PoolAdmin.delete_pool(pool_id, force)
-
-
-@router.patch(
-    "/{pool_id}/scale-up",
-    status_code=status.HTTP_200_OK,
-    description="Manually scale-up a browser-pool by its ID",
-    dependencies=[Depends(rate_limiter)],
-)
-async def manual_scale_up(pool_id: str):
-    pool = PoolAdmin.get_pool(pool_id)
-    if not pool:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Pool not found")
-
-    pool.create_new_browser()
-    return JSONResponse(status_code=status.HTTP_200_OK, content={"pool": pool.__repr__()})
-
-
-@router.patch(
-    "/{pool_id}/scale-down",
-    status_code=status.HTTP_200_OK,
-    description="Manually scale-down a browser-pool by its ID (forcefully terminates the least busy browser)",
-    dependencies=[Depends(rate_limiter)],
-)
-async def manual_scale_down(pool_id: str):
-    """
-    Manually scale-down a browser-pool by its ID (removes the least busy browser)
-    This operation is not reversible and will remove the least busy browser and all its sessions!
-    """
-    pool = PoolAdmin.get_pool(pool_id)
-    if not pool:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Pool not found")
-
-    if not await pool.remove_browser_by_id(pool.get_least_busy_browser().id_, force=True):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Browser not found")
-
-    return JSONResponse(status_code=status.HTTP_200_OK, content={"pool": pool.__repr__()})
